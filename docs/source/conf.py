2dca4b39113d0f5c935a3f8508d5a922b5674d4e
cutright <dan.cutright@gmail.com>
2021-03-10 16:44:09 -0600

    docs-specific requirements to avoid wx install

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -71,6 +71,8 @@ html_theme = "sphinx_rtd_theme"
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 
+html_short_title = 'IQDMA Docs'
+
 latex_elements = {"extraclassoptions": "openany,oneside"}
 
 autoclass_content = "both"
